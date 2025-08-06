84d1c49f4c5fe4088db30ff3e02505f18ce659c7
aidenlynnc-spec <aidenlynnc@gmail.com>
2025-08-06 13:36:33 -0500

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,10 +1,8 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.environ.get("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
